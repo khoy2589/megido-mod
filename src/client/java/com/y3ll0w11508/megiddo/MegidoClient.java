2d40089aeb7aaaefb68c202a38328383e9ef2d4d
Y3ll0W <kluai2589@gmail.com>
2026-01-09 11:49:03 +0700

    I want megido in minecraft

---
+++ src/client/java/com/y3ll0w11508/megiddo/MegidoClient.java
@@ -0,0 +1,10 @@
+package com.y3ll0w11508.megiddo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MegidoClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
